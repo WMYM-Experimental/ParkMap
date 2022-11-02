da27ebf4de37db81ca6043780fd2fc263e7aacb0
Washington Yandun <washington.yandun06@gmail.com>
2022-11-02 12:46:52 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ParkMap.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
